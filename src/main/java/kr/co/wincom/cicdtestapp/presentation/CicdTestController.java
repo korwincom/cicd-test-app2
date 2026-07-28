dd4cd92abbece99d1a2c465c9ec195db4813674b
권형도(윈컴) <wlegend04@lgupluspartners.cl.kr>
2026-07-28 17:08:15 +0900

    최초 등록

---
+++ src/main/java/kr/co/wincom/cicdtestapp/presentation/CicdTestController.java
@@ -0,0 +1,29 @@
+package kr.co.wincom.cicdtestapp.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "CI/CD 테스트", description = "CI/CD 테스트")
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CicdTestController {
+
+    @Operation(summary = "CI/CD 테스트", description = "CI/CD 테스트")
+    @GetMapping("hello")
+    public ResponseEntity<String> hello () {
+
+        // 결과 출력
+        log.info("Hello world from cicd-test-app2");
+        return ResponseEntity.ok("Hello world from cicd-test-app2");
+    }
+
+}
---
+++ src/test/java/kr/co/wincom/cicdtestapp/CicdTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wincom.cicdtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
